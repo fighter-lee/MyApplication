735c3e1fd19825c44e028a6b2afcb63256e386f9
fighter_lee <fighterlee1@gmail.com>
2020-08-27 20:52:48 +0800

    优化init

---
+++ app-optimize/src/main/java/com/fighter/app_optimize/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fighter.app_optimize;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
